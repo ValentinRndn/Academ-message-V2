1ea43a4d57fc58685eb60a9d868a99edf0fa79ce
Valentin <valentinrenaudin2206@gmail.com>
2025-05-19 23:18:18 +0200

    Add navigation link to dashboard on index page

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,14 @@
         <div class="animate-float-reverse absolute bottom-20 left-1/3 w-72 h-72 rounded-full bg-purple-100 opacity-40"></div>
       </div>
       
+      <NuxtLink 
+                to="/teachers" 
+                class="text-indigo-600 font-medium hover:text-indigo-800 inline-flex items-center group fixed top-3 
+                left-[400px]"
+              >
+                Go to dashboard
+              </NuxtLink>
+
       <div class="max-w-6xl mx-auto px-4 py-20 relative z-10">
         <div class="grid md:grid-cols-2 gap-12 items-center">
           <div class="animate-fade-in-up text-center md:text-left">
@@ -135,7 +143,7 @@
               <span class="inline-flex items-center justify-center w-12 h-12 rounded-full bg-indigo-100">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
+                </svg> 
               </span>
             </div>
           </div>
